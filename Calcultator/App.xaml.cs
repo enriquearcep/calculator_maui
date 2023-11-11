0003f8fd646cf77447c02fbe56aa6ab4f56802af
Enrique <enriquearce00@gmail.com>
2023-11-11 12:16:26 -0600

    Starting...

---
+++ Calcultator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Calcultator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Calcultator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Calcultator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
